85e145410bd593134e660ab0a4171c5a51d6189f
Leandro Pereira <leandro.nares@gmail.com>
2019-08-24 13:21:56 -0300

    Deve alterar conta com sucesso

--- BarrigaRest/src/main/java/br/ce/wcaquino/rest/tests/BarrTest.java
+++ BarrigaRest/src/main/java/br/ce/wcaquino/rest/tests/BarrTest.java
@@ -1,16 +1,38 @@
 package br.ce.wcaquino.rest.tests;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.ce.wcaquino.rest.core.BaseTest;
-import io.restassured.RestAssured;
 
 public class BarrTest extends BaseTest {
+	
+	private String TOKEN;
+	
+	@Before
+	public void login() {
+		
+	//Criando Map para enviar o token para ter acesso a aplicação e poder inserir uma conta
+		Map<String, String> login = new HashMap<>();
+		 login.put("email", "leandro.nares@gmail.com");
+		 login.put("senha", "123");	
+			//Variável que receberá o Token	
+		    TOKEN = given() 
+			//Enviado o usuário e senha para a API
+				.body(login)
+				.when()
+					.post("/signin")
+				.then()
+					.statusCode(200)
+					//Extraindo o Token
+					.extract().path("token");	
+	}
 
 	@Test
 	public void naoDeveAcessarApiSemToken() {	
@@ -26,27 +48,10 @@ public class BarrTest extends BaseTest {
 	@Test
 	public void deveIncluirContaComSucesso() {
 		
-		//Criando Map para enviar o token para ter acesso a aplicação e poder inserir uma conta
-		Map<String, String> login = new HashMap<>();
-		
-			login.put("email", "leandro.nares@gmail.com");
-			login.put("senha", "123");
-			
-		//Variável que receberá o Token	
-		String token = given() 
-			//Enviado o usuário e senha para a API
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			//Extraindo o Token
-			.extract().path("token"); 
-		
 		//Criando a conta após logar na aplicação
 		given()
 			//Enviado o token para a aplicação
-			.header("Authorization", "JWT " + token) //Aplicações mais recentes usam "bearer" no lugar de JWT
+			.header("Authorization", "JWT " + TOKEN) //Aplicações mais recentes usam "bearer" no lugar de JWT
 			//Enviado em um body o nome da conta que será cadastrada
 			.body("{\"nome\": \"conta qualquer\"}")
 		.when()
@@ -57,4 +62,17 @@ public class BarrTest extends BaseTest {
 		;		
 	}
 	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		given()
+			.header("Authorization", "JWT " + TOKEN) 
+			.body("{\"nome\": \"conta depois da alteração\"}")
+		.when()
+			.put("/contas/30893")// id da conta que fiz a alteração
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("nome", is("conta depois da alteração"))
+		;		
+	}	
 }
